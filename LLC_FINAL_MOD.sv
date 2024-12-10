4969aadbd91e38b9b94245d75b8ffbe6acc985c1
Tokibot1 <151477485+Tokibot1@users.noreply.github.com>
2024-12-09 22:35:37 -0800

    Update LLC_FINAL_MOD.sv

--- LLC_FINAL_MOD.sv
+++ LLC_FINAL_MOD.sv
@@ -280,7 +280,7 @@ task Snooped_read_request(input logic [ADDRESS_WIDTH-1:0] address);
     logic [INDEX_BITS-1:0] index;
     logic [OFFSET_BITS-1:0] offset;
     integer set_index, line_index;
-    integer hit_type = 4;
+    integer hit_type;
     
         decode_address(address, tag, index, offset);
         // Iterate over all cache lines in the set
@@ -300,13 +300,14 @@ task Snooped_read_request(input logic [ADDRESS_WIDTH-1:0] address);
                     cache.sets[index].lines[line_index].mesi_state = SHARED;
                    // PutSnoopResult(address, `HITM);
                     hit_type = 3;
+                end
 
                 else begin
                     hit_type = 4;
                 end 
             end
         end
-        end
+
         case(hit_type)
             1: begin
                 if(NormalMode) begin
@@ -355,7 +356,7 @@ task Snooped_RWIM_request(input logic [ADDRESS_WIDTH-1:0] address);
     logic [INDEX_BITS-1:0] index;
     logic [OFFSET_BITS-1:0] offset;
     integer set_index, line_index;
-    integer hit_type = 4;
+    integer hit_type;
     decode_address(address, tag, index, offset);
         // Iterate over all cache lines in the set
         for (line_index = 0; line_index < ASSOCIATIVITY; line_index = line_index + 1) begin
@@ -374,12 +375,13 @@ task Snooped_RWIM_request(input logic [ADDRESS_WIDTH-1:0] address);
                 else if (cache.sets[index].lines[line_index].mesi_state == MODIFIED) begin
                     cache.sets[index].lines[line_index].mesi_state = INVALID;
                     hit_type = 3;
+                end
                 else begin
                     hit_type = 4;
                 end
             end
         end
-        end
+
         case(hit_type)
             1: begin
                 if(NormalMode) begin
@@ -492,7 +494,9 @@ function integer select_victim_line(input logic [INDEX_BITS-1:0] set_index);
         for (level = 0; level < bit_length; level++) begin
             // Determine the bit1 to access based on the level
             bit1 = cache.sets[set_index].lru_state[node_index];
+            if (NormalMode) begin
             $display("Node: %0d LUR bits: %0d", node_index , bit1);
+            end
 
             if (bit1 == 1) begin
                 // Update the LRU bit1 to 0
